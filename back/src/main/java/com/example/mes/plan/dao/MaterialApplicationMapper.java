0f73633e9e021dc96bc3ac6d3ca8d3691563f01c
swz <673551955@qq.com>
2022-07-11 21:30:27 +0800

    生产计划管理

--- back/src/main/java/com/example/mes/plan/dao/MaterialApplicationMapper.java
+++ back/src/main/java/com/example/mes/plan/dao/MaterialApplicationMapper.java
@@ -3,9 +3,10 @@ package com.example.mes.plan.dao;
 import com.example.mes.plan.common.MesBaseMapper;
 import com.example.mes.plan.entity.MaterialApplication;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Set;
-
+@Repository
 public interface MaterialApplicationMapper extends MesBaseMapper<MaterialApplication> {
 	
 	Set<MaterialApplication> selectByPlanId(@Param("id") String id);
